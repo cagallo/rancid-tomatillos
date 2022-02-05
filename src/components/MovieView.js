af19657fa6feebef80b0213ccfe35adc95e39bd3
Cesare Gallo <78453792+cagallo@users.noreply.github.com>
2022-02-05 15:01:24 -0700

    Changed to class component

--- src/components/MovieView.js
+++ src/components/MovieView.js
@@ -1,20 +1,47 @@
 import React from 'react';
+import { Component } from 'react/cjs/react.production.min';
+import Trailer from './Trailer';
+import apiCalls from '../apiCalls'
+import {checkResponse} from '../utilities'
 import '../css/MovieView.css';
 
-const MovieView = ({ selectedMovie }) => {
-  return (
-      <section className='movie-view'>
-        <img src={selectedMovie.backdrop_path} alt="movie poster"/>
-        <h2>{selectedMovie.title}</h2>
-        <p>{selectedMovie.average_rating}</p>
-        <p>{selectedMovie.release_date}</p>
-        <p>{selectedMovie.overview}</p>
-        <p>{selectedMovie.genre}</p>
-        <p>{selectedMovie.budget}</p>
-        <p>{selectedMovie.revenue}</p>
-        <p>{selectedMovie.runtime}</p>
-      </section>
-  );
+class MovieView extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            trailer: '',
+            error: '',
+            isLoading: ''
+        }
+    }
+
+    componentDidMount = () => {
+        apiCalls.getTrailerData(this.props.selectedMovie.id)
+            .then(response => checkResponse(response))
+            .then(data => this.setState({trailer: data}))
+            .catch((error) => this.setState({error: error.message}))
+    }
+
+
+    render = () => { 
+        console.log(this.state.trailer)  
+        return (
+            <section className='movie-view'>
+            <Trailer />
+                <img src={this.props.selectedMovie.backdrop_path} alt="movie poster"/>
+                <h2>{this.props.selectedMovie.title}</h2>
+                <p>{this.props.selectedMovie.average_rating}</p>
+                <p>{this.props.selectedMovie.release_date}</p>
+                <p>{this.props.selectedMovie.overview}</p>
+                <p>{this.props.selectedMovie.genre}</p>
+                <p>{this.props.selectedMovie.budget}</p>
+                <p>{this.props.selectedMovie.revenue}</p>
+                <p>{this.props.selectedMovie.runtime}</p>
+            </section>
+        );
+    }
 }
+
+
 
 export default MovieView;

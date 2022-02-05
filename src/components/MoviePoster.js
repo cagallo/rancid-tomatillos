9ec120ec524f54282882824218ae8bd509a9eefe
Cesare Gallo <78453792+cagallo@users.noreply.github.com>
2022-02-04 18:06:06 -0700

    Add on click event

--- src/components/MoviePoster.js
+++ src/components/MoviePoster.js
@@ -1,9 +1,9 @@
 import React from "react";
 import "../css/MoviePoster.css";
 
-const MoviePoster = ({ title, rating, poster }) => {
+const MoviePoster = ({ title, rating, poster, id, handleClick }) => {
   return (
-    <section className="poster-frame">
+    <section className="poster-frame" onClick={(event => handleClick(event.target.id))} id={id}>
       <img src={poster} alt="movie" />
       <p>Rating: {parseFloat(rating).toFixed(1)} / 10</p>
       <h2>{title}</h2>

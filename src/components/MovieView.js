33ee85bedcd52b6bb90dd342dcf2973c57016fa8
Cesare Gallo <78453792+cagallo@users.noreply.github.com>
2022-02-05 16:14:22 -0700

    Add props to Trailer component

--- src/components/MovieView.js
+++ src/components/MovieView.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { Component } from 'react/cjs/react.production.min';
 import Trailer from './Trailer';
-import apiCalls from '../apiCalls'
-import {checkResponse} from '../utilities'
+import apiCalls from '../apiCalls';
+import {checkResponse} from '../utilities';
 import '../css/MovieView.css';
 
 class MovieView extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            trailer: '',
+            trailer: 'https://www.youtube.com/embed/',
             error: '',
             isLoading: ''
         }
@@ -18,17 +18,21 @@ class MovieView extends Component {
     componentDidMount = () => {
         apiCalls.getTrailerData(this.props.selectedMovie.id)
             .then(response => checkResponse(response))
-            .then(data => this.setState({trailer: data}))
+            .then(data => this.setState(prevState => {
+               return {trailer: `${prevState.trailer}${data.videos[0].key}`}
+                }))
             .catch((error) => this.setState({error: error.message}))
     }
+
+
 
 
     render = () => { 
         console.log(this.state.trailer)  
         return (
             <section className='movie-view'>
-            <Trailer />
-                <img src={this.props.selectedMovie.backdrop_path} alt="movie poster"/>
+                <Trailer src={this.state.trailer}/>
+                {/* <img src={this.props.selectedMovie.backdrop_path} alt="movie poster"/> */}
                 <h2>{this.props.selectedMovie.title}</h2>
                 <p>{this.props.selectedMovie.average_rating}</p>
                 <p>{this.props.selectedMovie.release_date}</p>
@@ -41,6 +45,7 @@ class MovieView extends Component {
         );
     }
 }
+
 
 
 

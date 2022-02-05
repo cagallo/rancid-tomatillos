db59ecb99723438f822f14a717afb8c821dcbbdc
Ethan Tweitmann <ethantweitmann@gmail.com>
2022-02-05 09:47:53 -0800

    Deconstruct props to clean dot notation path

--- src/components/MovieView.js
+++ src/components/MovieView.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import '../css/MovieView.css';
 
-const MovieView = (props) => {
-    return (
-        <section className='movie-view'>
-            <img src={props.selectedMovie.backdrop_path} alt="movie photo"/>
-            <h2>{props.selectedMovie.title}</h2>
-            <p>{props.selectedMovie.average_rating}</p>
-            <p>{props.selectedMovie.release_date}</p>
-        </section>
-    )
+const MovieView = ({ selectedMovie }) => {
+  return (
+      <section className='movie-view'>
+        <img src={selectedMovie.backdrop_path} alt="movie poster"/>
+        <h2>{selectedMovie.title}</h2>
+        <p>{selectedMovie.average_rating}</p>
+        <p>{selectedMovie.release_date}</p>
+        <p>{selectedMovie.overview}</p>
+        <p>{selectedMovie.genre}</p>
+        <p>{selectedMovie.budget}</p>
+        <p>{selectedMovie.revenue}</p>
+        <p>{selectedMovie.runtime}</p>
+      </section>
+  );
 }
 
 export default MovieView;

b83cd5f764387021b91d1c1f0c5ba73b126d44dc
Cesare Gallo <78453792+cagallo@users.noreply.github.com>
2022-02-05 14:58:25 -0700

    Add getMovieTrailer api call

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -7,7 +7,12 @@ const apiCalls = {
         `https://rancid-tomatillos.herokuapp.com/api/v2/movies/${id}`
       );
     }
+  },
+
+  getTrailerData(id) {
+    return fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${id}/videos`);
   }
+
 };
 
 export default apiCalls;

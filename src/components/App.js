9cc0ec695596dcb6465592d6dd039d1d4e6cba50
Ethan Tweitmann <ethantweitmann@gmail.com>
2022-02-05 09:20:32 -0800

    Add componentDidMount

--- src/components/App.js
+++ src/components/App.js
@@ -2,23 +2,32 @@ import React, {Component} from 'react';
 import Header from './Header';
 import MovieContainer from './MovieContainer';
 import MovieView from './MovieView';
-import movieData from '../movieData';
+//import movieData from '../movieData';
+import apiCalls from '../apiCalls';
 import '../css/App.css';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      movies: movieData,
+      movies: [],
       error: "",
       isLoading: true,
       selectedMovie: ''
     };
   }
 
+  componentDidMount = () => {
+    apiCalls.getAllMovies()
+      .then((response) => response.json())
+      .then((movies) => this.setState({ movies: movies.movies }))
+      .catch((error) => this.setState({ error: error.message }))
+      .finally(this.setState({ isLoading: false }));
+  };
+
   handleClick = (id) => {
     console.log(id);
-    const currentMovie = this.state.movies.movies.find(movie=>movie.id === parseInt(id));
+    const currentMovie = this.state.movies.find(movie=>movie.id === parseInt(id));
     console.log(currentMovie);
     this.setState({selectedMovie: currentMovie});
   }

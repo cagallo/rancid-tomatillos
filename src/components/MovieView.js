ed55484500db39b1f34f592a6d9b645a3dce2dd8
Cesare Gallo <78453792+cagallo@users.noreply.github.com>
2022-02-04 18:31:17 -0700

    Finish iteration 2

--- src/components/MovieView.js
+++ src/components/MovieView.js
@@ -1,18 +1,20 @@
-import { render } from '@testing-library/react';
-import React, {Component} from 'react';
+import React from 'react';
 import '../css/MovieView.css';
 
 
-class MovieView extends Component {
-    constructor(props) {
-        super(props);
-    }
 
-render() {
-        return (
-            <h1>Working!!!</h1>
-        )
-    }
+const MovieView = (props) => {
+    return (
+        <section className='movie-view'>
+            <img src={props.selectedMovie.backdrop_path} alt="movie photo"/>
+            <h2>{props.selectedMovie.title}</h2>
+            <p>{props.selectedMovie.average_rating}</p>
+            <p>{props.selectedMovie.release_date}</p>
+        </section>
+    )
 }
+
+
+
 
 export default MovieView;

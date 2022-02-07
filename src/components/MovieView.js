632062258c3fdd77e381ef3a6a70a3ba352d3d8b
Ethan Tweitmann <ethantweitmann@gmail.com>
2022-02-06 16:25:59 -0800

    Implement conditional rendering and fix bug with trailer fetch request

--- src/components/MovieView.js
+++ src/components/MovieView.js
@@ -18,13 +18,17 @@ class MovieView extends Component {
         apiCalls.getTrailerData(this.props.selectedMovie.id)
             .then(response => checkResponse(response))
             .then(data => this.setState(prevState => {
-               return {trailer: `${prevState.trailer}${data.videos[0].key}`}
+               if (!data.videos.length) {
+                 return
+               }
+               const key = data.videos.find(video => video.type === 'Trailer').key
+               return {trailer: `${prevState.trailer}${key}`}
                 }))
-            .catch((error) => this.setState({error: error.message}))
+            .catch((error) => this.setState({error: error.message}));
     }
 
     render = () => {
-        const trailerContent = this.state.error ? <h2>{this.state.error}</h2> : <Trailer className='movie-trailer' src={this.state.trailer}/>;
+        const trailerContent = this.state.error ? <h2 className="error-message">{this.state.error}</h2> : <Trailer backdropImg={this.props.selectedMovie.backdrop_path} src={this.state.trailer}/>;
         return (
             <section className='movie-view'>
                 {trailerContent}
@@ -33,16 +37,16 @@ class MovieView extends Component {
                         <img className="single-view-poster" src={this.props.selectedMovie.poster_path} alt="movie poster"/>
                         <div className='movie-info'>
                             <h2 className='single-view-title'>{this.props.selectedMovie.title}</h2>
-                            <p>{this.props.selectedMovie.average_rating}</p>
-                            <p>{this.props.selectedMovie.release_date}</p>
-                            <p>{this.props.selectedMovie.genre}</p>
-                            <p>{this.props.selectedMovie.budget}</p>
-                            <p>{this.props.selectedMovie.revenue}</p>
                             <p>{this.props.selectedMovie.runtime}</p>
+                            <p>Rating: {this.props.selectedMovie.average_rating} / 10</p>
+                            {this.props.selectedMovie.release_date && <p>Release Date: {this.props.selectedMovie.release_date}</p>}
+                            {this.props.selectedMovie.genre && <p>Genre: {this.props.selectedMovie.genre}</p>}
+                            {this.props.selectedMovie.budget && <p>Budget: {this.props.selectedMovie.budget}</p>}
+                            {this.props.selectedMovie.revenue && <p>Revenue: {this.props.selectedMovie.revenue}</p>}
                         </div>
                     </section>
                     <h3>Overview</h3>
-                    <p className='single-view-overview'>{this.props.selectedMovie.overview}</p>
+                    {this.props.selectedMovie.overview && <p className='single-view-overview'>{this.props.selectedMovie.overview}</p>}
                 </div>
             </section>
         );

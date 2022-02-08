4bc16ec16f1963032ac30b7136afdee721e9e700
Ethan Tweitmann <ethantweitmann@gmail.com>
2022-02-08 15:06:10 -0800

    Import and implement Link component

--- src/components/MovieContainer.js
+++ src/components/MovieContainer.js
@@ -1,18 +1,21 @@
 import React from "react";
 import MoviePoster from "./MoviePoster";
+import { Link } from "react-router-dom";
 import "../css/MovieContainer.css";
 
 const MovieContainer = ({ allMovies, handlePosterClick }) => {
-  const moviePosters = allMovies.map((movie) => (
-    <MoviePoster
-      key={movie.id}
-      title={movie.title}
-      rating={movie.average_rating}
-      poster={movie.poster_path}
-      id={movie.id}
-      handlePosterClick={handlePosterClick}
-    />
-  ));
+  const moviePosters = allMovies.map((movie) => {
+    return <Link to={`/movies/${movie.id}`}>
+      <MoviePoster
+        key={movie.id}
+        title={movie.title}
+        rating={movie.average_rating}
+        poster={movie.poster_path}
+        id={movie.id}
+        handlePosterClick={handlePosterClick}
+      />
+    </Link>
+  });
 
   return (
     <div className="movies-container">

14c90f888267751cc4e7d9fbdbc8d7b55ae05153
Cesare Gallo <78453792+cagallo@users.noreply.github.com>
2022-02-07 17:21:58 -0700

    Rename click event

--- src/components/MoviePoster.js
+++ src/components/MoviePoster.js
@@ -1,9 +1,9 @@
 import React from "react";
 import "../css/MoviePoster.css";
 
-const MoviePoster = ({ title, rating, poster, id, handleClick }) => {
+const MoviePoster = ({ title, rating, poster, id, handlePosterClick }) => {
   return (
-    <section className="poster-frame" onClick={(event => handleClick(event.target.id))} id={id}>
+    <section className="poster-frame" onClick={(event => handlePosterClick(event.target.id))} id={id}>
       <img className="poster" src={poster} alt="movie" />
       <p className="poster-banner">Rating: {parseFloat(rating).toFixed(1)} / 10</p>
       <h2 className="poster-banner">{title}</h2>

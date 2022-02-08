cb6c4c2b9c25abcc2309348787f7793854326a4f
Cesare Gallo <78453792+cagallo@users.noreply.github.com>
2022-02-07 17:21:19 -0700

    Change data type for movies and rename click event

--- src/components/App.js
+++ src/components/App.js
@@ -13,7 +13,7 @@ class App extends Component {
       movies: [],
       error: "",
       isLoading: true,
-      selectedMovie: ''
+      selectedMovie: {}
     };
   }
 
@@ -30,7 +30,7 @@ class App extends Component {
       .catch((error) => this.setState({ error: error.message }));
   };
 
-  handleClick = (id) => {
+  handlePosterClick = (id) => {
     apiCalls.getMovieData(id)
       .then((response) => checkResponse(response))
       .then(({ movie }) => {
@@ -42,10 +42,10 @@ class App extends Component {
 
   render = () => {
     let currentView;
-    if (this.state.selectedMovie) {
+    if (Object.keys(this.state.selectedMovie).length > 0) {
       currentView = <MovieView selectedMovie={this.state.selectedMovie}/>
     } else {
-      currentView = this.state.error ? <h2 className="landing-page-error-message">{this.state.error}</h2> : <MovieContainer allMovies={this.state.movies} handleClick={this.handleClick}/>
+      currentView = this.state.error ? <h2 className="landing-page-error-message">{this.state.error}</h2> : <MovieContainer allMovies={this.state.movies} handlePosterClick={this.handlePosterClick}/>
     }
     return (
       <section className="App">

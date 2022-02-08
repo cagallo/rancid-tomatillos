46559275ab2ce616cbca758dff61b1c54d11c795
Ethan Tweitmann <ethantweitmann@gmail.com>
2022-02-07 17:04:06 -0800

    Install Cypress and create acceptance test for Rancid Tomatillos landing page

---
+++ cypress/integration/landing_page_spec.js
@@ -0,0 +1,27 @@
+describe('Rancid Tomatillos landing page flow', () => {
+
+  it('should display the page title and all the movie posters', () => {
+    cy.intercept('https://rancid-tomatillos.herokuapp.com/api/v2/movies', {
+      "movies": [
+        {
+          "id": 694919,
+          "poster_path": "https://image.tmdb.org/t/p/original//6CoRTJTmijhBLJTUNoVSUNxZMEI.jpg",
+          "backdrop_path": "https://image.tmdb.org/t/p/original//pq0JSpwyT2URytdFG0euztQPAyR.jpg",
+          "title": "Money Plane",
+          "average_rating": 6.625,
+          "release_date": "2020-09-29"
+        },
+      ]
+    })
+    cy.visit('http://localhost:3000')
+      .get('h1')
+      .contains('Rancid Tomatillos')
+      .get('.movies-container')
+      .get('.poster-frame')
+      .get('img')
+      .get('p')
+        .contains('Rating: 6.6 / 10')
+      .get('h2')
+        .contains('Money Plane');
+  });
+});

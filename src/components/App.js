1b2cbf63dafb94500497d2b90aa6d124dcb93bf7
Ethan Tweitmann <ethantweitmann@gmail.com>
2022-02-09 15:26:44 -0800

    Refactor App using react router

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import Header from './Header';
 import MovieContainer from './MovieContainer';
 import MovieView from './MovieView';
 import apiCalls from '../apiCalls';
-import { cleanMovieData, formatAverageRating } from "../utilities.js";
+import { formatAverageRating } from "../utilities.js";
 import { Route } from 'react-router-dom';
 import '../css/App.css';
 
@@ -13,7 +13,6 @@ class App extends Component {
     this.state = {
       movies: [],
       error: "",
-      isLoading: true,
       selectedMovie: {},
       trailer: 'https://www.youtube.com/embed/'
     };
@@ -41,30 +40,30 @@ class App extends Component {
   //     .catch((error) => this.setState({ error: error.message }));
   // };
 
-  handlePosterClick = async(id) => {
-    try {
-        const requests = [apiCalls.getMovieData(id), apiCalls.getTrailerData(id)]
-        const data = await Promise.all(requests)
-        let [movie, videos] = [data[0], data[1].videos];
-        const cleanedMovieData = cleanMovieData(movie.movie);
-        if (!videos.length) {
-            return this.setState({ selectedMovie: { ...movie.movie, ...cleanedMovieData }})
-        }
-        const key = videos.find(video => video.type === 'Trailer').key
-        this.setState(prevState => {
-          return {
-            selectedMovie:  {...movie.movie, ...cleanedMovieData},
-            trailer: `${prevState.trailer}${key}`,
-          }
-        });
-    }
-    catch(error) {
-        this.setState({ error: error.message });
-    }
-  }
+  // handlePosterClick = async(id) => {
+  //   try {
+  //       const requests = [apiCalls.getMovieData(id), apiCalls.getTrailerData(id)]
+  //       const data = await Promise.all(requests)
+  //       let [movie, videos] = [data[0], data[1].videos];
+  //       const cleanedMovieData = cleanMovieData(movie.movie);
+  //       if (!videos.length) {
+  //           return this.setState({ selectedMovie: { ...movie.movie, ...cleanedMovieData }})
+  //       }
+  //       const key = videos.find(video => video.type === 'Trailer').key
+  //       this.setState(prevState => {
+  //         return {
+  //           selectedMovie:  {...movie.movie, ...cleanedMovieData},
+  //           trailer: `${prevState.trailer}${key}`,
+  //         }
+  //       });
+  //   }
+  //   catch(error) {
+  //       this.setState({ error: error.message });
+  //   }
+  // }
 
 
-  // handlePosterClick = (id) => {
+  // componentDidUpdate = (id) => {
   //   let movie, videos, cleanedMovieData, key;
   //   Promise.all([
   //   apiCalls.getMovieData(id),
@@ -74,7 +73,7 @@ class App extends Component {
   //       [movie, videos] = [data[0], data[1].videos];
   //       cleanedMovieData = cleanMovieData(movie.movie);
   //       if(!videos.length) {
-  //         return this.setState({ selectedMovie: { ...movie, ...cleanedMovieData }})
+  //         return this.setState({ selectedMovie: { ...movie.movie, ...cleanedMovieData }})
   //       } else {
   //         key = videos.find(video => video.type === 'Trailer').key
   //         this.setState(prevState => {
@@ -88,26 +87,23 @@ class App extends Component {
   //     .catch((error) => this.setState({ error: error.message }));
   // };
 
-
   render = () => {
-    let currentView;
-    if (Object.keys(this.state.selectedMovie).length > 0) {
-     currentView = <MovieView trailer={this.state.trailer} selectedMovie={this.state.selectedMovie}/>
-    } else {
-      currentView = this.state.error ? <h2 className="landing-page-error-message">{this.state.error}</h2> : <MovieContainer allMovies={this.state.movies} handlePosterClick={this.handlePosterClick}/>
-    }
+    // let currentView;
+    // if (Object.keys(this.state.selectedMovie).length > 0) {
+    //  currentView = <MovieView trailer={this.state.trailer} selectedMovie={this.state.selectedMovie}/>
+    // } else {
+    //   currentView = this.state.error ? <h2 className="landing-page-error-message">{this.state.error}</h2> : <MovieContainer allMovies={this.state.movies} handlePosterClick={this.handlePosterClick}/>
+    // }
     return (
       <section className="App">
         <Header />
-        <Route path='/movies/:id' render={({ match }) => {
-          const selectedMovie = this.state.movies.find(movie => movie.id === match.params.id)
-          return <MovieView trailer={this.state.trailer} selectedMovie={selectedMovie}/>
+        <Route path='/:id' render={({ match }) => {
+          return <MovieView id={match.params.id} />
         }} />
-        <Route exact path='/' render={() => {
+        <Route exact path='/'>
           {this.state.error && <h2 className="landing-page-error-message">{this.state.error}</h2>}
-          <MovieContainer allMovies={this.state.movies} handlePosterClick={this.handlePosterClick}/>
-        }} />
-        {currentView}
+          <MovieContainer allMovies={this.state.movies} />
+        </Route>
       </section>
     );
   }

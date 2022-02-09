a2b9a07d610d27945a4d5873679d9d8bbd4fd8c0
Ethan Tweitmann <ethantweitmann@gmail.com>
2022-02-09 15:27:58 -0800

    Add Link component and remove handleClick prop

--- src/components/MovieContainer.js
+++ src/components/MovieContainer.js
@@ -3,16 +3,15 @@ import MoviePoster from "./MoviePoster";
 import { Link } from "react-router-dom";
 import "../css/MovieContainer.css";
 
-const MovieContainer = ({ allMovies, handlePosterClick }) => {
+const MovieContainer = ({ allMovies }) => {
   const moviePosters = allMovies.map((movie) => {
-    return <Link to={`/movies/${movie.id}`}>
+    return <Link to={`/${movie.id}`}>
       <MoviePoster
         key={movie.id}
         title={movie.title}
         rating={movie.average_rating}
         poster={movie.poster_path}
         id={movie.id}
-        handlePosterClick={handlePosterClick}
       />
     </Link>
   });

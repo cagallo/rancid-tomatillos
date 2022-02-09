2578545f2c3997edbab92460302ca1d45cb5f834
Ethan Tweitmann <ethantweitmann@gmail.com>
2022-02-09 15:29:14 -0800

    Remove onClick prop

--- src/components/MoviePoster.js
+++ src/components/MoviePoster.js
@@ -1,9 +1,9 @@
 import React from "react";
 import "../css/MoviePoster.css";
 
-const MoviePoster = ({ title, rating, poster, id, handlePosterClick }) => {
+const MoviePoster = ({ title, rating, poster, id }) => {
   return (
-    <section className="poster-frame" onClick={(event => handlePosterClick(event.target.id))} id={id}>
+    <section className="poster-frame" id={id}>
       <img className="poster" src={poster} alt="movie" />
       <p className="poster-banner">Rating: {parseFloat(rating).toFixed(1)} / 10</p>
       <h2 className="poster-banner">{title}</h2>

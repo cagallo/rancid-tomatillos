05f703839bb56a25f06a61f461855f3d8396cdd2
Ethan Tweitmann <ethantweitmann@gmail.com>
2022-02-12 11:24:21 -0800

    Reorder intercepts

--- cypress/integration/movie_view_spec.js
+++ cypress/integration/movie_view_spec.js
@@ -71,19 +71,6 @@ describe('Rancid Tomatillos selected movie flow', () => {
     })
 
     it('should allow user to return to the landing page', () => {
-        cy.intercept('https://rancid-tomatillos.herokuapp.com/api/v2/movies', {
-            "movies": [
-              {
-                "id": 694919,
-                "poster_path": "https://image.tmdb.org/t/p/original//6CoRTJTmijhBLJTUNoVSUNxZMEI.jpg",
-                "backdrop_path": "https://image.tmdb.org/t/p/original//pq0JSpwyT2URytdFG0euztQPAyR.jpg",
-                "title": "Money Plane",
-                "average_rating": 6.625,
-                "release_date": "2020-09-29"
-              },
-            ]
-          })
-
          cy.intercept('https://rancid-tomatillos.herokuapp.com/api/v2/movies/694919', {
             "movie": {
                 "id": 694919,
@@ -114,6 +101,19 @@ describe('Rancid Tomatillos selected movie flow', () => {
                 }
             ]
         }).as('getSingleMovieTrailer')
+
+        cy.intercept('https://rancid-tomatillos.herokuapp.com/api/v2/movies', {
+            "movies": [
+              {
+                "id": 694919,
+                "poster_path": "https://image.tmdb.org/t/p/original//6CoRTJTmijhBLJTUNoVSUNxZMEI.jpg",
+                "backdrop_path": "https://image.tmdb.org/t/p/original//pq0JSpwyT2URytdFG0euztQPAyR.jpg",
+                "title": "Money Plane",
+                "average_rating": 6.625,
+                "release_date": "2020-09-29"
+              },
+            ]
+          })
 
         cy.visit('http://localhost:3000/694919');
         cy.wait(['@getSingleMovie', '@getSingleMovieTrailer']);

b1b4ebeb6e08b03a4840cee735cbd07c6006ee9c
Ethan Tweitmann <ethantweitmann@gmail.com>
2022-02-12 15:00:57 -0800

    Convert strings to use single quotes

--- cypress/integration/landing_page_spec.js
+++ cypress/integration/landing_page_spec.js
@@ -2,18 +2,18 @@ describe('Rancid Tomatillos landing page flow', () => {
 
   it('should display the page title and all the movie posters', () => {
     cy.intercept('https://rancid-tomatillos.herokuapp.com/api/v2/movies', {
-      "movies": [
+      'movies': [
         {
-          "id": 694919,
-          "poster_path": "https://image.tmdb.org/t/p/original//6CoRTJTmijhBLJTUNoVSUNxZMEI.jpg",
-          "backdrop_path": "https://image.tmdb.org/t/p/original//pq0JSpwyT2URytdFG0euztQPAyR.jpg",
-          "title": "Money Plane",
-          "average_rating": 6.625,
-          "release_date": "2020-09-29"
+          'id': 694919,
+          'poster_path': 'https://image.tmdb.org/t/p/original//6CoRTJTmijhBLJTUNoVSUNxZMEI.jpg',
+          'backdrop_path': 'https://image.tmdb.org/t/p/original//pq0JSpwyT2URytdFG0euztQPAyR.jpg',
+          'title': 'Money Plane',
+          'average_rating': 6.625,
+          'release_date': '2020-09-29'
         },
       ]
     });
-    
+
     cy.visit('http://localhost:3000');
     cy.location().should((loc) => {
       expect(loc.href).to.eq('http://localhost:3000/');

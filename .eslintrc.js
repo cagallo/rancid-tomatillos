3b9332bf5ba555a0b82733e23b599efb4c374eae
Ethan Tweitmann <ethantweitmann@gmail.com>
2022-02-12 16:54:07 -0800

    Install cypress eslint plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,8 +3,14 @@ module.exports = {
 		'browser': true,
 		'es2021': true
 	},
+	"settings": {
+    "react": {
+      "version": "detect"
+    }
+  },
 	'extends': [
 		'eslint:recommended',
+		'plugin:cypress/recommended',
 		'plugin:react/recommended'
 	],
 	'parserOptions': {

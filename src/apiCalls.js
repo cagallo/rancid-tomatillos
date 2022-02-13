77cd5356cc9fc4f180d86b814bd31f70066b0389
Ethan Tweitmann <ethantweitmann@gmail.com>
2022-02-12 16:58:05 -0800

    Fix errors pointed out by linter

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,14 +1,14 @@
-import { checkResponse } from "./utilities";
+import { checkResponse } from './utilities';
 
 const apiCalls = {
   async getMovieData (id) {
-    let url = "https://rancid-tomatillos.herokuapp.com/api/v2/movies/";
-    if(id) {
-      url += `${id}`
+    let url = 'https://rancid-tomatillos.herokuapp.com/api/v2/movies/';
+    if (id) {
+      url += `${id}`;
     }
     let response = await fetch(url);
-      checkResponse(response);
-      return await response.json();
+    checkResponse(response);
+    return await response.json();
   },
 
   async getTrailerData(id) {
@@ -16,7 +16,6 @@ const apiCalls = {
     checkResponse(response);
     return await response.json();
   }
-  
 };
 
 export default apiCalls;
